7f24809487124e452b4f0e59e0eb237490130dcd
joas-vieira <joas.vieira.dev@gmail.com>
2025-06-02 19:41:37 -0300

    test: should register a new user successfully

--- src/http/controllers/register.e2e.ts
+++ src/http/controllers/register.e2e.ts
@@ -1,7 +1,19 @@
-import { describe, expect, it } from 'vitest';
+import { app } from '@/app';
+import request from 'supertest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
-describe('Register E2E Tests', () => {
+describe('Register (e2e)', () => {
+  beforeAll(async () => await app.ready());
+
+  afterAll(async () => await app.close());
+
   it('should register a new user successfully', async () => {
-    expect(true).toBe(true); // Placeholder for actual test logic
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: '123456'
+    });
+
+    expect(response.statusCode).toBe(201);
   });
 });
